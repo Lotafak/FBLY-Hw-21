fb0aabd7d01feb5a5e37671845937fe7c239e746
Patryk Kudła <patt.kudla@gmail.com>
2022-04-01 13:57:17 +0300

    Implements front-end data fetching and displaying

---
+++ frontend/src/components/NpsGroups.vue
@@ -0,0 +1,61 @@
+<template>
+  <StatsContainer :displayData="getData" title="NPS Groups" type="groups">
+    <div v-if="data" class="nps-groups-container">
+      <span
+        >Promoters: <span class="promoters">{{ data.promoters }}</span></span
+      >
+      <span
+        >Passives: <span class="passives">{{ data.passives }}</span></span
+      >
+      <span
+        >Detractors: <span class="detractors">{{ data.detractors }}</span></span
+      >
+    </div>
+    <span v-if="!data">No data yet</span>
+  </StatsContainer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import StatsContainer from "@/components/StatsContainer.vue";
+import { NpsGroupsType } from "@/api";
+
+function getData(this: any, data?: NpsGroupsType) {
+  if (data) {
+    this.data = data;
+  }
+}
+
+export default defineComponent({
+  name: "NpsGroups",
+  components: {
+    StatsContainer,
+  },
+  methods: {
+    getData,
+  },
+  data() {
+    return { data: null };
+  },
+});
+</script>
+
+<style>
+.nps-groups-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.promoters {
+  color: green;
+}
+
+.passives {
+  color: darkgoldenrod;
+}
+
+.detractors {
+  color: red;
+}
+</style>
---
+++ frontend/src/components/NpsScore.vue
@@ -0,0 +1,31 @@
+<template>
+  <StatsContainer :displayData="getData" title="NPS Score" type="score">
+    <h2 v-if="data">{{ data }}</h2>
+    <span v-if="!data">No data yet</span>
+  </StatsContainer>
+</template>
+
+<script lang="ts">
+import { ComponentOptions, defineComponent } from "vue";
+import StatsContainer from "@/components/StatsContainer.vue";
+import { NpsScoreType } from "@/api";
+
+function getData(this: any, data?: NpsScoreType) {
+  if (data) {
+    this.data = data.score;
+  }
+}
+
+export default defineComponent({
+  name: "NpsScore",
+  components: {
+    StatsContainer,
+  },
+  methods: {
+    getData,
+  },
+  data() {
+    return { data: null };
+  },
+});
+</script>
